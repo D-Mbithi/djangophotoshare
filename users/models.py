1a582d803915211285df06f6cea1b553d4f0f946
dennis mbithi <denmbithi@gmail.com>
2022-08-19 21:55:30 +0300

    Feature: Configured custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+    def __str__(self):
+        full_name = f"{self.first_name} {self.last_name}".title()
+        
+        return full_name
